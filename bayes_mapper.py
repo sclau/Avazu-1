b6fffe782dc0bc50795310d71838df840ede2a29
sclau <sclau@berkeley.edu>
2014-11-15 23:56:24 -0800

    MR counts and probs fix

--- bayes_mapper.py
+++ bayes_mapper.py
@@ -1,14 +1,19 @@
-#!usr/bin/python
+#!/usr/bin/python
 # python bayes_mapper.py < split2_small_train.txt > counts_small_train.txt
 
 import sys
 
+fields = ["banner_pos", "site_id", "site_domain", "site_category", "app_id","app_domain", "app_category", "device_id", "device_ip", "device_os", "device_make", "device_model", "device_type", "device_conn_type", "device_geo_country"]
+
 for line in sys.stdin:
     line = line.strip()
     values = line.split('\t')
-    print len(values)
     if len(values) == 28:
         if values[0] == "train":
-            features = [values[1]]
-            features.extend(values[5:20])
-            print '\t'.join(features)
+            click = values[2]  # click or no-click
+            features = values[5:20]
+            for i in range(0, len(features)):
+                key = click + "." + fields[i] + "." + features[i]
+                print "%s\t%s" % (key, 1)
+            # count total of clicks and no-clicks
+            print "%s\t%s" % (click + ".total.total", 1)
--- bayes_prob.py
+++ bayes_prob.py
@@ -1,12 +1,17 @@
-#!usr/bin/python
-# python bayes_prob.py < bcounts_small_train.txt
+#!/usr/bin/python
+# python bayes_prob.py < bcounts_small_train.txt > bprob_small_train.txt
+
+# cat $(ls -t) > avazu-train.txt
+# python bayes_prob.py < ./data/out4/avazu-train.txt > bprob-avazu-train.txt
 
 import sys
 
-counts = {"0": {}, "1": {}}
+fields = ["banner_pos", "site_id", "site_domain", "site_category", "app_id","app_domain", "app_category", "device_id", "device_ip", "device_os", "device_make", "device_model", "device_type", "device_conn_type", "device_geo_country"]
+
+counts = {"0": {"total": 0.0}, "1": {"total": 0.0}}
 for i in range(0, 15):
-    counts["0"][str(i)] = {"feature_ct": 0.0, "strange": 0.0}
-    counts["1"][str(i)] = {"feature_ct": 0.0, "strange": 0.0}
+    counts["0"][fields[i]] = {"feature_ct": 0.0, "strange": 0.0}
+    counts["1"][fields[i]] = {"feature_ct": 0.0, "strange": 0.0}
     
 for line in sys.stdin:
     line = line.strip()
@@ -14,16 +19,14 @@ for line in sys.stdin:
     if len(values) == 4:
         click, feature, cat, count = values
         ct = float(count)
-        counts[click][feature]["feature_ct"] += ct
-        if ct < 6:
-            counts[click][feature]["strange"] += ct
+        if feature != "total":
+            counts[click][feature]["feature_ct"] += ct
+            if ct < 6:
+                counts[click][feature]["strange"] += ct
+            else:
+                counts[click][feature][cat] = ct
         else:
-            counts[click][feature][cat] = ct
-                
-    elif len(values) == 3:
-        click, total, count = values
-        if total == "total":
-            counts[click]["total"] = float(count)
+            counts[click]["total"] += ct
 
 prob_click = counts["1"]["total"] / (counts["1"]["total"] + counts["0"]["total"])
             
@@ -31,15 +34,18 @@ for click in counts:
     for feature in counts[click]:
         if feature != "total":
             feature_ct = counts[click][feature]["feature_ct"]
+            # unique # of categories in feature. subtract "feature_ct" & "strange"
+            unique_cat = len(set(counts[click][feature].keys())) - 2
             for cat in counts[click][feature]:
                 if cat != "feature_ct":
                     cat_ct = counts[click][feature][cat]
-                    prob = cat_ct / feature_ct
+                    # +1 and +1 unique_cat bc we want probabilities with smoothing
+                    prob = (cat_ct + 1) / (feature_ct + unique_cat)
                     print "%s\t%s\t%s\t%s\t%s" % (click, feature, cat, prob, cat_ct)
                 else:
                     print "%s\t%s\t%s" % (click, feature, feature_ct)
         else:
             if click == '0':  
-                print "%s\t%s\t%s\t%s" % (click, feature, prob_click, counts[click]["total"])
-            else:
                 print "%s\t%s\t%s\t%s" % (click, feature, (1.0 - prob_click), counts[click]["total"])
+            else:
+                print "%s\t%s\t%s\t%s" % (click, feature, prob_click, counts[click]["total"])
